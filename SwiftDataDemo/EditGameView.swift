868457b4d5a4fa5fe451e74a57d868ba7a70c66b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-11-13 17:10:00 -0500

    Create EditGameView file

---
+++ SwiftDataDemo/EditGameView.swift
@@ -0,0 +1,19 @@
+//
+//  EditGameView.swift
+//  SwiftDataDemo
+//
+//  Created by Leonard McCook-Carr on 11/13/23.
+//
+
+import SwiftUI
+
+struct EditGameView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditGameView()
+}
