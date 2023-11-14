b778f37a717b3c2ae93b860134b4be158624db3c
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-11-14 08:41:58 -0500

    Create State variables for each property

--- SwiftDataDemo/EditGameView.swift
+++ SwiftDataDemo/EditGameView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct EditGameView: View {
+//    let game: Game
+    @State private var status = Status.inBacklog
+    @State private var rating: Int?
+    @State private var title = ""
+    @State private var developer = ""
+    @State private var summary = ""
+    @State private var dateAdded = Date.distantPast
+    @State private var dateStarted = Date.distantPast
+    @State private var dateCompleted = Date.distantPast
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
