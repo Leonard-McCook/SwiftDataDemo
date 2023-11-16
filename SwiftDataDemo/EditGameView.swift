3e1e9415b17c6db41898570553eb6ec923f8fd30
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-11-16 13:51:12 -0500

    Add Datepicker with if statements

--- SwiftDataDemo/EditGameView.swift
+++ SwiftDataDemo/EditGameView.swift
@@ -17,6 +17,7 @@ struct EditGameView: View {
     @State private var dateAdded = Date.distantPast
     @State private var dateStarted = Date.distantPast
     @State private var dateCompleted = Date.distantPast
+    @State private var firstView = true
     
     var body: some View {
         HStack {
@@ -29,7 +30,50 @@ struct EditGameView: View {
             .buttonStyle(.bordered)
         }
         VStack(alignment: .leading) {
-            
+            GroupBox {
+                LabeledContent {
+                    DatePicker("", selection: $dateAdded, displayedComponents: .date)
+                } label: {
+                     Text("Date Added")
+                }
+                if status == .inProgress || status == .completed {
+                    LabeledContent {
+                        DatePicker("", selection: $dateStarted, displayedComponents: .date)
+                    } label: {
+                         Text("Date Started")
+                    }
+                }
+                if status == .completed {
+                    LabeledContent {
+                        DatePicker("", selection: $dateCompleted, displayedComponents: .date)
+                    } label: {
+                         Text("Date Completed")
+                    }
+                }
+            }
+            .foregroundStyle(.secondary)
+            .onChange(of: status) { oldValue, newValue in
+                if !firstView {
+                    if newValue == .inBacklog {
+                        dateStarted = Date.distantPast
+                        dateCompleted = Date.distantPast
+                    } else if newValue == .inProgress && oldValue == .completed {
+                        // from completed to inProgress
+                        dateCompleted = Date.distantPast
+                    } else if newValue == .inProgress && oldValue == .inBacklog {
+                        // Game has been started
+                        dateStarted = Date.now
+                    } else if newValue == .completed && oldValue == .inBacklog {
+                        // Forgot to start game
+                        dateCompleted = Date.now
+                        dateStarted = dateAdded
+                    } else {
+                        // completed
+                        dateCompleted = Date.now
+                    }
+                    firstView = false
+                }
+            }
         }
     }
 }
